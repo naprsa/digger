9dedf58a16f4378ee1775049f3dd4f97ccc0f665
Cupera <napr.sa@gmail.com>
2020-12-21 02:11:48 +0300

    upd

---
+++ src/assets/tiles/ground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="ground" tilewidth="50" tileheight="50" tilecount="400" columns="20">
+ <image source="ground_tilesest/ground.png" width="1024" height="1024"/>
+</tileset>
